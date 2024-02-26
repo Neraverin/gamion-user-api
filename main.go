6ce7087f8cc45ec388ea896a6c55d66246ea740e
Vladimir Salykin <neraverin@gmail.com>
2024-02-26 21:43:56 +0300

    Now example server will provide simple Users API

--- main.go
+++ main.go
@@ -6,47 +6,45 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var users = []user{
+	{ID: "1", Name: "John Coltrane"},
+	{ID: "2", Name: "Gerry Mulligan"},
+	{ID: "3", Name: "Sarah Vaughan"},
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getUserByID)
+	router.POST("/users", postUsers)
 
 	router.Run("localhost:8080")
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
 }
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+func postUsers(c *gin.Context) {
+	var newUser user
 
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := c.BindJSON(&newUser); err != nil {
 		return
 	}
 
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	users = append(users, newUser)
+	c.IndentedJSON(http.StatusCreated, newUser)
 }
 
-func getAlbumByID(c *gin.Context) {
+func getUserByID(c *gin.Context) {
 	id := c.Param("id")
 
-	for _, a := range albums {
+	for _, a := range users {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
